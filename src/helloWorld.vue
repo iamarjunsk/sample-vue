d6e0251fd02b45d22250efe688d37a885bec2c98
Arjun SK <dev.arjunsk@gmail.com>
2020-09-26 19:24:07 +0530

    methods

---
+++ src/helloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Dev: {{developer}}</h3>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            developer:"Arjun SK"
+        };
+    },
+    methods:{
+        heloWld(){
+            alert("helo Arjun");
+            this.developer="Sk"
+        }
+    },
+    mounted(){
+        console.log(this);
+        this.heloWld()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
